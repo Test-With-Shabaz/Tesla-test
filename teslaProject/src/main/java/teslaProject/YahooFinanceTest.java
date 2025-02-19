8b842fec0a59abf4cd832d0382f4279ec477ff4f
Test-With-Shabaz <shabazmohammed4838@gmail.com>
2025-02-19 19:37:03 +0530

    Add files via upload

---
+++ teslaProject/src/main/java/teslaProject/YahooFinanceTest.java
@@ -0,0 +1,63 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+public class YahooFinanceTest {
+    public static void main(String[] args) {
+    	
+        // Set up WebDriver (Make sure chromedriver is in your system path)
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shaba\\Downloads\\chromedriver-win64\\chromedriver");
+        WebDriver driver = new ChromeDriver();
+        
+        try {
+            // Navigate to Yahoo Finance
+            driver.get("https://finance.yahoo.com/");
+            
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+            // Search for Tesla stock (TSLA)
+            WebElement searchBox = driver.findElement(By.id("ybar-sbq"));
+            
+            searchBox.sendKeys("TSLA");
+            Thread.sleep(2000); 
+
+            // Verify autosuggest contains "TESLA Inc."
+            WebElement firstSuggestion = driver.findElement(By.xpath("//ul[@role='listbox']//li[1]"));
+            
+            if (firstSuggestion.getText().contains("TESLA Inc.")) {
+                System.out.println("Autosuggest verified.");
+            } else {
+                System.out.println("Autosuggest verification failed.");
+            }
+
+            firstSuggestion.click();
+            
+            Thread.sleep(3000);
+
+            WebElement stockPriceElement = driver.findElement(By.xpath("//fin-streamer[@data-field='regularMarketPrice']"));
+            
+            double stockPrice = Double.parseDouble(stockPriceElement.getText().replace(",", ""));
+            if (stockPrice > 200) {
+                System.out.println("Stock price verification passed: " + stockPrice);
+            } else {
+                System.out.println("Stock price verification failed: " + stockPrice);
+            }
+
+            WebElement previousClose = driver.findElement(By.xpath("//td[@data-test='PREV_CLOSE-value']"));
+            
+            WebElement volume = driver.findElement(By.xpath("//td[@data-test='TD_VOLUME-value']"));
+            
+            System.out.println("Previous Close: " + previousClose.getText());
+            
+            System.out.println("Volume: " + volume.getText());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           
+            driver.quit();
+        }
+    }
+}
