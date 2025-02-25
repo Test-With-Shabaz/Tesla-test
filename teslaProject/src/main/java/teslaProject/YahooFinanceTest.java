800d64fb16df6abdc30f574e33a0b9d357237f9d
Test-With-Shabaz <shabazmohammed4838@gmail.com>
2025-02-25 08:07:57 -0800

    Update YahooFinanceTest.java

--- teslaProject/src/main/java/teslaProject/YahooFinanceTest.java
+++ teslaProject/src/main/java/teslaProject/YahooFinanceTest.java
@@ -10,53 +10,47 @@ public class YahooFinanceTest {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         
-        try {
-            // Navigate to Yahoo Finance
-            driver.get("https://finance.yahoo.com/");
-            
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       try {
+	            // Navigate to Yahoo Finance
+	            driver.get("https://finance.yahoo.com/");
+	            
+	            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-            // Search for Tesla stock (TSLA)
-            WebElement searchBox = driver.findElement(By.id("ybar-sbq"));
-            
-            searchBox.sendKeys("TSLA");
-            Thread.sleep(2000); 
+	            // Search for Tesla stock (TSLA)
+	            WebElement searchBox = driver.findElement(By.id("ybar-sbq"));
+	            
+	            searchBox.sendKeys("TSLA");
+	            Thread.sleep(2000); 
 
-            // Verify autosuggest contains "TESLA Inc."
-            WebElement firstSuggestion = driver.findElement(By.xpath("//ul[@role='listbox']//li[1]"));
-            
-            if (firstSuggestion.getText().contains("TESLA Inc.")) {
-                System.out.println("Autosuggest verified.");
-            } else {
-                System.out.println("Autosuggest verification failed.");
-            }
+	            // Verify autosuggest contains "TESLA Inc."
+	            WebElement firstSuggestion = driver.findElement(By.xpath("(//ul[@role='listbox']/li/div/div/div[contains(@class,'modules-module_quoteCompanyName')])[1]"));
+	            
+	            firstSuggestion.click();
+	            
+	            Thread.sleep(3000);
 
-            firstSuggestion.click();
-            
-            Thread.sleep(3000);
+	            WebElement stockPriceElement = driver.findElement(By.xpath("//section[@data-testid='quote-price']//span[@data-testid='qsp-price']"));
+	            
+	            double stockPrice = Double.parseDouble(stockPriceElement.getText());
+	            if (stockPrice > 200) {
+	                System.out.println("Stock price verification passed: " + stockPrice);
+	            } else {
+	                System.out.println("Stock price verification failed: " + stockPrice);
+	            }
 
-            WebElement stockPriceElement = driver.findElement(By.xpath("//fin-streamer[@data-field='regularMarketPrice']"));
-            
-            double stockPrice = Double.parseDouble(stockPriceElement.getText().replace(",", ""));
-            if (stockPrice > 200) {
-                System.out.println("Stock price verification passed: " + stockPrice);
-            } else {
-                System.out.println("Stock price verification failed: " + stockPrice);
-            }
+	            WebElement previousClose = driver.findElement(By.xpath("//fin-streamer[@data-field='regularMarketPreviousClose']"));
+	            
+	            WebElement volume = driver.findElement(By.xpath("//fin-streamer[@data-field='regularMarketVolume']/span"));
+	            
+	            System.out.println("Previous Close: " + previousClose.getText());
+	            
+	            System.out.println("Volume: " + volume.getText());
 
-            WebElement previousClose = driver.findElement(By.xpath("//td[@data-test='PREV_CLOSE-value']"));
-            
-            WebElement volume = driver.findElement(By.xpath("//td[@data-test='TD_VOLUME-value']"));
-            
-            System.out.println("Previous Close: " + previousClose.getText());
-            
-            System.out.println("Volume: " + volume.getText());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-           
-            driver.quit();
-        }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        } finally {
+	           
+	            driver.quit();
+	        }
     }
 }
